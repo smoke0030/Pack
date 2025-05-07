c6811745437707ae2500d13de3da1505f5179a9b
Admin <ft.priest@gmail.com>
2025-05-07 16:00:07 +0300

    update

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppTokenKit",
-            url: "https://github.com/smoke0030/AppTokenKit/releases/download/1.0.4/AppTokenKit.xcframework.zip",
-            checksum: "9154af66c24feb2311d14cc7907f17fcb145e8c8a20c9a05c0fb5ee04544f50f"
+            url: "https://github.com/smoke0030/AppTokenKit/releases/download/1.0.5/AppTokenKit.xcframework.zip",
+            checksum: "465320ba18fc108acd1f2ca5764f0d0b4f13172753a459cf8e7255c727269cea"
         )
     ]
 )
