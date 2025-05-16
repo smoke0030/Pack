9b7f2c4c4458745d6d599257e58733299407276f
Admin <ft.priest@gmail.com>
2025-05-16 14:53:19 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppTokenKit",
-            url: "https://github.com/smoke0030/AppTokenKit/releases/download/1.0.7/AppTokenKit.xcframework.zip",
-            checksum: "9bfc4390885e8c9cfd46525d5121bc262bcced2c7a5d9ed5d20c1ec575f646ca"
+            url: "https://github.com/smoke0030/AppTokenKit/releases/download/1.0.8/AppTokenKit.xcframework.zip",
+            checksum: "0297ad8837f56f73c75147b289181f0cc5ea455ae04e616f933c6471ff92fa91"
         )
     ]
 )
